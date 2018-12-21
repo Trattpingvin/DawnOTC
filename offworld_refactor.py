10b4c6cee5d74fe6a5af38c6f4dd1e15b97f2e22
Rhahi <rhahi@hanmail.net>
2018-12-22 02:31:56 +0900

    same data simulation and summary

--- offworld_refactor.py
+++ offworld_refactor.py
@@ -36,13 +36,13 @@ def rating_init(matches):
         for i, r in enumerate(results):
             players[p[i].lower()].update_ts(r[0]) #rating is 1 length tuple
 
+    return players
 
 def weight_star_bracket(players):
     """Create list of obejcts needed for rest of the program
     get ratings: sort it using their weighted rank, and assign brackets, etc.
     return dict of Player() objects.
     """
-
     #sorting data to divide brackets
     sorted_ratings = sorted(players.items(), key=lambda kv: kv[1], reverse=True)
 
@@ -56,7 +56,6 @@ def weight_star_bracket(players):
         print(v)
         pass
     """
-    return players
 
 class Player:
     sigma_weight = 2 #Trueskill uses 3 by default; we use 2 here.
@@ -94,20 +93,24 @@ class Player:
         self.ts = r
         self.rnk = r.mu - self.sigma_weight * r.sigma
 
-
-def run_a_match(match, p):
+def run_a_match(match, p, s):
     """given single match data, return resulting player information.
     get [[list of players], [list of win data]]
     return dictionary of players with updated bracket
     """
+
+    #setup
     players, wins = match
     players = [n.lower() for n in players]
+
+    #trueskill update
     players_ts = [(p[n].ts,) for n in players]
     rating_result = ts.rate(players_ts, ranks=wins)
     for i, r in enumerate(rating_result):
         p[ players[i] ].update_ts(r[0])
 
-    for idx, name in enumerate(players):
+    #main loop
+    for idx, n in enumerate(players):
         my_level = p[n].bracket
         my_stars = p[n].stars
         my_stars_before = my_stars
@@ -131,10 +134,10 @@ def run_a_match(match, p):
             if my_stars_before == 0 and my_level > locked_level:
                     my_stars = default_lose_star
                     my_level = my_level - 1
-                    leveldown += 1
+                    s["leveldown"] += 1
             else:
                 my_stars = 0
-                locked += 1
+                s["locked"] += 1
 
         elif my_stars > max_star:
             #star_overflow = max_star - my_stars
@@ -145,96 +148,24 @@ def run_a_match(match, p):
             else:
                 my_stars = default_win_star
                 my_level = my_level + 1
-                levelup += 1
+                s["levelup"] += 1
 
-        p[name].bracket = my_level
-        p[name].stars = my_stars
-        p[name].awards += awards
+        #update values
+        p[n].bracket = my_level
+        p[n].stars = my_stars
+        p[n].awards += awards
+        s["awards"] += awards
 
-    
+    return s
 
 def simulate_matches(matches, p):
     count = 0
-    levelup = 0
-    leveldown = 0
-    locked = 0
-
+    s = {"levelup": 0, "leveldown": 0, "locked": 0, "awards": 0}
+    
     for match in matches:
-        players, wins = match
-        brackets = [ p[name][0] for name in players ]
-        #p[name]: 0: bracket 1: star 2: medals 3: truskill rating 4: weighted rating
-
-        for idx, name in enumerate(players):
-            my_level = p[name][0]
-            my_stars = p[name][1]
-            my_stars_before = my_stars
-            other_level = max(brackets[:idx] + brackets[idx+1:])
-            winner_level = p[players[wins.index(0)]]
-            awards = 0
-
-            if wins[idx] == 0: #win
-                #default bonus + bonus for defeating higher level player
-                high_level_bonus = max(0, other_level - my_level)
-                my_stars += win_bonus + high_level_bonus
-                if high_level_bonus > 0:
-                    awards += 1
-
-            elif wins[idx] == 1: #lose
-                #default malus
-                my_stars += lose_malus #+ min(0, winner_level - my_level)
-
-            if my_stars < 0:
-                #only lose a level when lost at star 0.
-                if my_stars_before == 0 and my_level > locked_level:
-                        my_stars = default_lose_star
-                        my_level = my_level - 1
-                        leveldown += 1
-                else:
-                    my_stars = 0
-                    locked += 1
-
-            elif my_stars > max_star:
-                #star_overflow = max_star - my_stars
-                #if my_level == 1: print("level up!")
-                if my_level == max_level:
-                    #awards += 1
-                    my_stars = max_star
-                else:
-                    my_stars = default_win_star
-                    my_level = my_level + 1
-                    levelup += 1
-                
-
-            p[name][0] = my_level
-            p[name][1] = my_stars
-            p[name][2] += awards
+        run_a_match(match, p, s)
         count += 1
-
-    return p, [count, levelup, leveldown, locked]
-
-def analyze_results(data):
-    brackets = [0] * max_level
-    awards = 0
-    for _, v in data.items():
-        brackets[v[0] - 1] += 1
-        awards += v[2]
-        
-    print("Level status:", brackets, "from 1 to 5")
-    print("Awards:", awards)
-
-#control constants
-max_level = 5 #number of brackets
-assigned_max_level = 3
-locked_level = 1 #cannot drop below this level
-
-max_star = 3
-starting_stars = 2 #default stars given at start
-default_lose_star = 3
-default_win_star = 1
-
-win_bonus = 2
-lose_malus = -1
-
+    return s
 
 """ TODO
     Matchmaking (spits out new matches using the current brackets & availability)
@@ -255,6 +186,33 @@ lose_malus = -1
 def generate_matches(players, availability):
     pass
 
+def simulation_summary(players, summary=None, lowest_level = 5):
+    brackets = [0] * max_level
+    sorted_players = sorted(players.items(), key=lambda kv:kv[1], reverse=True)
+    count = 0
+    for v in sorted_players:
+        if v[1].bracket >= lowest_level:
+            print(v[1])
+            count += 1
+    print("\t\t\t\t\tDisplaying {} players".format(count))
+
+    for _, v in players.items():
+        brackets[v.bracket - 1] += 1
+    print("Level status:", brackets)
+    print(summary)
+
+#control constants
+max_level = 5 #number of brackets
+assigned_max_level = 3
+locked_level = 1 #cannot drop below this level
+
+max_star = 3
+starting_stars = 2 #default stars given at start
+default_lose_star = 3
+default_win_star = 1
+
+win_bonus = 2
+lose_malus = -1
 
 if __name__ == "__main__":
     data1 = get_data("ABTdata1.txt")
@@ -262,19 +220,14 @@ if __name__ == "__main__":
     players = rating_init(data1 + data2)
     weight_star_bracket(players) #alters players
 
-    """
-    for k, v in seed.items():
-        #if v[0] > 0: print(v, k)
-        pass
-
-    analyze_results(seed)
-    simulation_results, c = simulate_matches(data1 + data2, seed)
+    simulation_summary(players)
     print()
-    for k, v in simulation_results.items():
-        if v[0] > 0: print(v, k)
-        pass
 
-    analyze_results(simulation_results)
-    print("Total matches:", c[0])
-    print("Level Up-Down:", c[1], '/', c[2], '/ Freezed', c[3])
     """
+    matches = generate_matches(players, availability)
+
+    """
+    matches = data1 + data2
+    summary = simulate_matches(matches, players)
+    simulation_summary(players, summary=summary, lowest_level=1)
+
